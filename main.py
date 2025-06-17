f25d7fc207a97dcfce337fec6c7eb633c54661bd
YadaSarakoon <yadasarakoon0446@gmail.com>
2025-06-17 14:31:26 +0700

    init project

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def error():
+    return {"message": "Hello World"}
+
+@app.get("/todos")
+async def get_todos(): 
+    return [
+        {"id": 1, "detail": "first todos"},
+        {"id": 2, "detail": "seconds todos"}
+    ]
+
+counter = 0
+@app.get("/counter")
+async def get_counter():
+    global counter
+    counter += 1
+    return {"message" : f"counter = {counter}"}
